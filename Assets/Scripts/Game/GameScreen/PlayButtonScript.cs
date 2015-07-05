ea385c1689350189305e488c91e855f11366d818
Bernat Martinez <bernatmv@gmail.com>
2015-07-06 01:43:43 +0200

    (feat) barebones of play button

---
+++ Assets/Scripts/Game/GameScreen/PlayButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using com.lovelydog.movieschallenge;
+
+public class PlayButtonScript : FacadeMonoBehaviour {
+
+	public Color category1Color;
+	public Color category2Color;
+	public Color category3Color;
+	public Color category4Color;
+	public Color category5Color;
+	public Color category6Color;
+
+	public void play() {
+		// TODO: call API, begin showing category spin, trigger the category specified in the API call
+	}
+}
