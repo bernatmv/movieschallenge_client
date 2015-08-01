e54815c5833e7cfb5c0d24dad9dd75f64ce591d4
Bernat Martinez <bernatmv@gmail.com>
2015-08-01 12:49:15 +0200

    (feat) added radio buttons to select category when suggesting a challenge on the contribute section

---
+++ Assets/Scripts/Game/Contribute/CategoryInputTextScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using com.lovelydog;
+using com.lovelydog.movieschallenge;
+
+public class CategoryInputTextScript : FacadeMonoBehaviour {
+	
+	public InputField inputText;
+
+	void Awake() {
+		inputText.text = "0";
+		_dispatcher.AddListener ("category_check", updateInput);
+	}
+
+	public void updateInput(Object data) {
+		int checkedCategory = ((PayloadObject)data).intPayload;
+		inputText.text = checkedCategory + "";
+	}
+}
---
+++ Assets/Scripts/Game/Contribute/CategoryRadioScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using com.lovelydog;
+using com.lovelydog.movieschallenge;
+
+public class CategoryRadioScript : FacadeMonoBehaviour {
+
+	public Text label;
+	public Image bgImage;
+	public CanvasGroup bgCanvas;
+	public CanvasGroup check;
+	public int category;
+
+	void Awake() {
+		label.text = _i18n.get ("CATEGORY_" + category);
+		bgImage.color = Properties.categoriesColor [category - 1];
+		_dispatcher.AddListener ("category_check", uncheckThis);
+	}
+
+	public void checkThis() {
+		check.alpha = 1f;
+		bgCanvas.alpha = 0f;
+		_dispatcher.Dispatch ("category_check", new PayloadObject (category));
+	}
+
+	public void uncheckThis(Object data) {
+		int checkedCategory = ((PayloadObject)data).intPayload;
+		if (category != checkedCategory) {
+			bgCanvas.alpha = 1f;
+			check.alpha = 0f;
+		}
+	}
+}
